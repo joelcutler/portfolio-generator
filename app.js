ddc537c2ff6f20a19a78955ebcd5e51e65133b07
Joel C <joel7921111@yahoo.com>
2022-03-17 17:23:58 -0600

    broken, used  code snapshot

--- app.js
+++ app.js
@@ -1,235 +1,104 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-// const mockData = {
-//     name: 'Lernantino',
-//     github: 'lernantino',
-//     confirmAbout: true,
-//     about: 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-//     projects: [{
-//             name: 'Run Buddy',
-//             description: 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//             languages: ['HTML', 'CSS'],
-//             link: 'https://github.com/lernantino/run-buddy',
-//             feature: true,
-//             confirmAddProject: true
-//         },
-//         {
-//             name: 'Taskinator',
-//             description: 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//             languages: ['JavaScript', 'HTML', 'CSS'],
-//             link: 'https://github.com/lernantino/taskinator',
-//             feature: true,
-//             confirmAddProject: true
-//         },
-//         {
-//             name: 'Taskmaster Pro',
-//             description: 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//             languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-//             link: 'https://github.com/lernantino/taskmaster-pro',
-//             feature: false,
-//             confirmAddProject: true
-//         },
-//         {
-//             name: 'Robot Gladiators',
-//             description: 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-//             languages: ['JavaScript'],
-//             link: 'https://github.com/lernantino/robot-gladiators',
-//             feature: false,
-//             confirmAddProject: false
-//         }
-//     ]
-// };
-
-const promptUser = () => {
-    return inquirer.prompt([{
-            type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username (Required)',
-            validate: githubNameInput => {
-                if (githubNameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub Username!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAbout',
-            message: 'Would you like to enter some information about yourself for an "About" section?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself:',
-            when: ({ confirmAbout }) => {
-                if (confirmAbout) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-    ]);
-};
-
-const promptProject = portfolioData => {
-    console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
-
-    // If there's no 'projects' array property, create one
-    if (!portfolioData.projects) {
-        portfolioData.projects = [];
+// create the about section
+const generateAbout = aboutText => {
+    if (!aboutText) {
+        return '';
     }
-    return inquirer
-        .prompt([{
-                type: 'input',
-                name: 'name',
-                message: 'What is the name of your project? (Required)',
-                validate: projectNameInput => {
-                    if (projectNameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter your project name!');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Provide a description of the project (Required)',
-                validate: projectDescriptionInput => {
-                    if (projectDescriptionInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter a description of the project!');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'checkbox',
-                name: 'languages',
-                message: 'What did you build this project with? (Check all that apply)',
-                choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-            },
-            {
-                type: 'input',
-                name: 'link',
-                message: 'Enter the GitHub link to your project. (Required)',
-                validate: gitHubLinkInput => {
-                    if (gitHubLinkInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter the GitHub link to your project!');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'confirm',
-                name: 'feature',
-                message: 'Would you like to feature this project?',
-                default: false
-            },
-            {
-                type: 'confirm',
-                name: 'confirmAddProject',
-                message: 'Would you like to enter another project?',
-                default: false
-            }
-        ])
-        .then(projectData => {
-            portfolioData.projects.push(projectData);
-            if (projectData.confirmAddProject) {
-                return promptProject(portfolioData);
-            } else {
-                return portfolioData;
-            }
-        });
+
+    return `
+      <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+      </section>
+    `;
 };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        // const pageHTML = generatePage(mockData);
-        const pageHTML = generatePage();
+// create the projects section
+const generateProjects = projectsArr => {
+    return `
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+          .filter(({ feature }) => feature)
+          .map(({ name, description, languages, link }) => {
+            return ` <
+        div class = "col-12 mb-2 bg-dark text-light p-3" >
+        <
+        h3 class = "portfolio-item-title text-light" > $ { name } < /h3> <
+        h5 class = "portfolio-languages" >
+        Built With:
+        $ { languages.map(language => language).join(',') } <
+        /h5> <
+        p > $ { description } < /p> <
+        a href = "${link}"
+    class = "btn" > < i class = "fab fa-github mr-2" > < /i>View Project on GitHub</a >
+        <
+        /div>
+    `;
+          })
+          .join('')}
+  
+        ${projectsArr
+          .filter(({ feature }) => !feature)
+          .map(({ name, description, languages, link }) => {
+            return ` <
+    div class = "col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column" >
+        <
+        h3 class = "portfolio-item-title text-light" > $ { name } < /h3> <
+        h5 class = "portfolio-languages" >
+        Built With:
+        $ { languages.join(', ') } <
+        /h5> <
+        p > $ { description } < /p> <
+        a href = "${link}"
+    class = "btn mt-auto" > < i class = "fab fa-github mr-2" > < /i>View Project on GitHub</a >
+        <
+        /div>
+    `;
+          })
+          .join('')}
+        </div>
+      </section>
+    `;
+};
 
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
+// export function to generate entire page
+module.exports = templateData => {
+    // destructure page data by section
+    const { projects, about, ...header } = templateData;
 
-        });
-    });
-
-
-// console.log(inquirer)
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-
-// const pageHTML = generatePage(name, github);
-// const profileDataArgs = process.argv.slice(2);
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-
-// const [name, github] = profileDataArgs;
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-
-// console.log(profileDataArgs);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-// if (err) throw err;
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-// if (err) throw new Error(err);
-
-// console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
-
-
-
-
-
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('================');
-
-//     // Is the same as this...
-//     profileDataArr.forEach((profileItem) => {
-//         console.log(profileItem)
-//     });
-
-//     console.log('================');
-
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+          <nav class="flex-row">
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+              header.github
+            }">GitHub</a>
+          </nav>
+        </div>
+      </header>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateProjects(projects)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
+    `;
+};
