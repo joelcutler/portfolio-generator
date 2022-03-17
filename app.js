11a7d89ed2f56e429858d9bc8a16770efd80a6d7
Joel C <joel7921111@yahoo.com>
2022-03-17 14:15:36 -0600

    added gitignore, started prompting for ?s

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
+
+
+// console.log(inquirer)
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+
+// const pageHTML = generatePage(name, github);
+// const profileDataArgs = process.argv.slice(2);
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 
 // console.log(profileDataArgs);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+// if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+// if (err) throw new Error(err);
+
+// console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
