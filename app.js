fe31f4a1aca03798e1fac02fd67e2ead8f812b30
Joel C <joel7921111@yahoo.com>
2022-03-20 12:16:24 -0600

    wrote and copied files using promises, then exported the functions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,11 +130,42 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+
+
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     const pageHTML = generatePage(portfolioData);
+
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('Page created! Check out index.html in this directory to see it!');
+      
+//         fs.copyFile('./src/style.css', './dist/style.css', err => {
+//           if (err) {
+//             console.log(err);
+//             return;
+//           }
+//           console.log('Style sheet copied successfully!');
+//         });
+//       });
+//     });
